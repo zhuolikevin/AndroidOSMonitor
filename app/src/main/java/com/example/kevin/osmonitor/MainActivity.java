13d85b37095355ebcd12bca023945d0a1e4a815d
Zhuo li <lizhuogo@gmail.com>
2015-12-08 16:14:34 -0500

    now we read cpu usage and overall memory usage

--- app/src/main/java/com/example/kevin/osmonitor/MainActivity.java
+++ app/src/main/java/com/example/kevin/osmonitor/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.kevin.osmonitor;
 
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
+import android.app.ActivityManager.MemoryInfo;
 import android.app.ListActivity;
 import android.net.TrafficStats;
 import android.os.Bundle;
+import android.os.Debug;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
@@ -12,6 +15,8 @@ import android.widget.Toast;
 
 import com.example.kevin.osmonitor.adapters.ListAdapter;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,7 +25,7 @@ public class MainActivity extends ListActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-//    private final ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+// private final ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
 
     private HashMap<String, Integer> processMap = new HashMap<String, Integer>();
 
@@ -53,6 +58,15 @@ public class MainActivity extends ListActivity {
 
         setListAdapter(new ListAdapter(this, allProcesses));
 
+        // Get overall memory usage
+        MemoryInfo mi = new MemoryInfo();
+        manager.getMemoryInfo(mi);
+        Log.d(TAG, "Available memory (MB): " + mi.availMem / 1048576L);
+        Log.d(TAG, "Total memory (MB): " + mi.totalMem / 1048576L);
+
+        float cpuUsage = readUsage();
+        Log.d(TAG, "CPU usage: " + cpuUsage);
+
     }
 
     @Override
@@ -62,8 +76,43 @@ public class MainActivity extends ListActivity {
 
         Log.d(TAG, "on click: " + process);
 //        Log.d(TAG, "This pid: " + pid);
-//
-        // Display data
+
+//        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+
         Toast.makeText(getApplicationContext(), "PID " + pid, Toast.LENGTH_LONG).show();
+    }
+
+    private float readUsage() {
+        try {
+            RandomAccessFile reader = new RandomAccessFile("/proc/stat", "r");
+            String load = reader.readLine();
+
+            String[] toks = load.split(" +");  // Split on one or more spaces
+
+            long idle1 = Long.parseLong(toks[4]);
+            long cpu1 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[5])
+                    + Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+
+            try {
+                Thread.sleep(360);
+            } catch (Exception e) {}
+
+            reader.seek(0);
+            load = reader.readLine();
+            reader.close();
+
+            toks = load.split(" +");
+
+            long idle2 = Long.parseLong(toks[4]);
+            long cpu2 = Long.parseLong(toks[2]) + Long.parseLong(toks[3]) + Long.parseLong(toks[5])
+                    + Long.parseLong(toks[6]) + Long.parseLong(toks[7]) + Long.parseLong(toks[8]);
+
+            return (float)(cpu2 - cpu1) / ((cpu2 + idle2) - (cpu1 + idle1));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return 0;
     }
 }
